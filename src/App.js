a900b7c4255fe8d186a9c8466f8113fd11f8240f
Adrian Amatriain <amatriain.133735@e.unavarra.es>
2024-03-19 16:44:16 +0100

    Footer sin hacer y Header casi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './Componentes/Interfaz/Header';
+import Footer from './Componentes/Interfaz/Footer';
 import { Button } from 'react-bootstrap';
+
+const footer = 'Adiós Mundo';
 
 function App() {
   return (
     <div className="App">
+      <Header header />
       <Button className='primary'>Hola</Button>
+      <Footer footer={footer} />
     </div>
   );
 }
---
+++ src/Componentes/Interfaz/Header.js
@@ -0,0 +1,35 @@
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import logo from '../Imagenes/logo.png';
+
+function Header() {
+    return (
+        <>
+            <Navbar bg="custom" expand="lg" style={{ backgroundColor: '#D8EAC7' }}>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="100"
+                        height="100"
+                        className="d-inline-block align-text"
+                        alt="Logo de la Tienda"
+                    />
+                    Nombre Tienda
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Form className="d-flex mx-auto">
+                        <FormControl
+                            type="search"
+                            placeholder="Buscar"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Buscar</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    );
+}
+
+export default Header;
