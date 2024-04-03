6770784a64bfcf5fe0e8b77ccf5208934e903014
Julia Pagola <pagola.132511@e.unavarra.es>
2024-04-03 19:28:36 +0200

    Quitar console.log

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
-import ListadoProductos from './Componentes/Productos/ListadoProductos';
-import DetalleProducto from './Componentes/Productos/DetalleProducto';
-import Header from './Componentes/Interfaz/Header';
-import Footer from './Componentes/Interfaz/Footer';
-import { Route, Routes } from 'react-router-dom';
-import Carrito from './Componentes/Productos/Carrito';
-import Contact from './Paginas/Contact';
-import AboutUs from './Paginas/AboutUs';
-import Error from './Paginas/Error';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import ListadoProductos from "./Componentes/Productos/ListadoProductos";
+import DetalleProducto from "./Componentes/Productos/DetalleProducto";
+import Header from "./Componentes/Interfaz/Header";
+import Footer from "./Componentes/Interfaz/Footer";
+import { Route, Routes } from "react-router-dom";
+import Carrito from "./Componentes/Productos/Carrito";
+import Contact from "./Paginas/Contact";
+import AboutUs from "./Paginas/AboutUs";
+import Error from "./Paginas/Error";
+import { useEffect, useState } from "react";
 
 function App() {
-
   const [carrito, setCarrito] = useState(() => {
-    const carritoGuardado = localStorage.getItem('carrito');
+    const carritoGuardado = localStorage.getItem("carrito");
     return carritoGuardado ? JSON.parse(carritoGuardado) : [];
   });
 
   useEffect(() => {
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   }, [carrito]);
 
   const comprobarCarrito = (producto) => {
     let encontrado = false;
-    carrito.forEach(elemento => {
+    carrito.forEach((elemento) => {
       if (elemento.id === producto.id) {
         producto.cantidad = elemento.cantidad;
         encontrado = true;
       }
-    })
+    });
     return encontrado;
   };
 
@@ -39,16 +38,16 @@ function App() {
           if (elemento.id === producto.id) {
             return {
               ...elemento,
-              cantidad: elemento.cantidad + 1
+              cantidad: elemento.cantidad + 1,
             };
           }
           return elemento;
-        })
-      })
+        });
+      });
     } else {
       setCarrito((prevCarrito) => {
         return [...prevCarrito, producto];
-      })
+      });
     }
   };
 
@@ -58,7 +57,7 @@ function App() {
         if (elemento.id === producto.id && elemento.cantidad > 0) {
           return {
             ...elemento,
-            cantidad: elemento.cantidad - 1
+            cantidad: elemento.cantidad - 1,
           };
         }
         return elemento;
@@ -70,44 +69,69 @@ function App() {
     setCarrito((prevCarrito) => {
       return prevCarrito.filter((elemento) => {
         return elemento.id !== producto.id;
-      })
-    })
+      });
+    });
   };
 
-  const vaciarCarrito = () =>{
+  const vaciarCarrito = () => {
     setCarrito([]);
-  }
+  };
 
   const accionCarrito = (accion, producto) => {
     if (!comprobarCarrito(producto)) {
       producto.cantidad = 1;
     }
-    console.log(producto.cantidad);
-    if (accion === 'añadir') {
+    if (accion === "añadir") {
       añadirCarrito(producto);
-    } 
-    else if (accion === 'eliminar' || producto.cantidad <=1) {
+    } else if (accion === "eliminar" || producto.cantidad <= 1) {
       eliminarCarrito(producto);
-    }
-    else if (comprobarCarrito(producto) && accion === 'quitar') {
+    } else if (comprobarCarrito(producto) && accion === "quitar") {
       quitarCarrito(producto);
     }
   };
 
-  
-  const [showMenuCarrito, setShowMenuCarrito] = useState(false);  
+  const [showMenuCarrito, setShowMenuCarrito] = useState(false);
 
   return (
     <div className="App">
-      <Header showMenuCarrito={showMenuCarrito} setShowMenuCarrito={setShowMenuCarrito} carrito={carrito}/>
+      <Header
+        showMenuCarrito={showMenuCarrito}
+        setShowMenuCarrito={setShowMenuCarrito}
+        carrito={carrito}
+      />
 
       <Routes>
-        <Route path='/' element={<ListadoProductos accionCarrito={accionCarrito} setShowMenuCarrito={setShowMenuCarrito} />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/carrito' element={<Carrito accionCarrito={accionCarrito} vaciarCarrito={vaciarCarrito} carrito={carrito} />} />
-        <Route path='/about-us' element={<AboutUs />} />
-        <Route path='/detalle-producto' element={<DetalleProducto accionCarrito={accionCarrito} setShowMenuCarrito={setShowMenuCarrito}/>} />
-        <Route path='*' element={<Error />} />
+        <Route
+          path="/"
+          element={
+            <ListadoProductos
+              accionCarrito={accionCarrito}
+              setShowMenuCarrito={setShowMenuCarrito}
+            />
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/carrito"
+          element={
+            <Carrito
+              accionCarrito={accionCarrito}
+              vaciarCarrito={vaciarCarrito}
+              carrito={carrito}
+            />
+          }
+        />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route
+          path="/detalle-producto"
+          element={
+            <DetalleProducto
+              accionCarrito={accionCarrito}
+              setShowMenuCarrito={setShowMenuCarrito}
+            />
+          }
+        />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </div>
