bc76bbde04eb8e45c08af2e0e488f97974294f43
Julia Pagola <pagola.132511@e.unavarra.es>
2024-04-09 23:43:47 +0200

    Incluir breadcrums en el header

--- src/Componentes/Interfaz/Header.js
+++ src/Componentes/Interfaz/Header.js
@@ -1,24 +1,48 @@
-import { Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar} from "react-bootstrap";
 import logo from "../Imagenes/logo.png";
+import login from "../Imagenes/login.png";
+import logout from "../Imagenes/logout.png";
 import carrito from "../Imagenes/carrito.png";
 import OpcionesDesplegable from "./OpcionesDesplegable";
 import CarritoDesplegable from "../Productos/CarritoDesplegable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// import { contextAut } from "../Autenticacion/contextAut";
 
 function Header(props) {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const contextAut = {
+    login: true,
+    userID: "userID",
+  };
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const [imagen, setImagen] = useState({ login });
+  const [url, setUrl] = useState("/login");
+  const [texto, setTexto] = useState("");
+
+  useEffect(() => {
+    if (contextAut.login) {
+      setImagen(logout);
+      setUrl("/logout");
+      setTexto("Usuario: " + contextAut.userID);
+    } else {
+      setImagen(login);
+      setUrl("/login");
+      setTexto("");
+    }
+  }, [contextAut.login, contextAut.userID]);
+
   return (
     <>
       <Navbar
-        className="mb-4"
+        className="mb-4 justify-content-between"
         bg="custom"
         expand="lg"
         style={{ backgroundColor: "#D8EAC7" }}>
@@ -30,36 +54,38 @@ function Header(props) {
             height="100"
             className="d-inline-block align-text"
             alt="Logo de la Tienda"
-          />
+            />
           Scader
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Form className="d-flex mx-auto">
-            <FormControl
-              type="search"
-              placeholder="Buscar"
-              className="mr-2"
-              aria-label="Search"
+        <div style={{textAlign:"center"}}>
+            <p>{texto}</p>
+          <Navbar.Brand
+            className="p-2"
+            onClick={() => props.setShowMenuCarrito(true)}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{
+              boxShadow: isHovered ? " 0px 0px 10px rgba(0, 0, 0, 0.5)" : "",
+            }}
+            role="button">
+            <img
+              src={carrito}
+              width="50"
+              height="50"
+              className="d-inline-block align-text"
+              alt="Carrito de la compra"
             />
-            <Button variant="outline-success">Buscar</Button>
-          </Form>
-        </Navbar.Collapse>
-        <Navbar.Brand
-        className="p-2"
-          onClick={() => props.setShowMenuCarrito(true)}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          style={{ boxShadow: isHovered ? " 0px 0px 10px rgba(0, 0, 0, 0.5)" : "" }}
-          role="button">
-          <img
-            src={carrito}
-            width="50"
-            height="50"
-            className="d-inline-block align-text"
-            alt="Carrito de la compra"
-          />
-        </Navbar.Brand>
+          </Navbar.Brand>
+          <Navbar.Brand href={url} role="button" className="p-2">
+            <img
+              src={imagen}
+              width="50"
+              height="50"
+              className="d-inline-block align-text m-2"
+              alt="Iniciar sesion"
+            />
+          </Navbar.Brand>
+        </div>
       </Navbar>
       <CarritoDesplegable
         showMenuCarrito={props.showMenuCarrito}
--- src/Componentes/Interfaz/OpcionesDesplegable.js
+++ src/Componentes/Interfaz/OpcionesDesplegable.js
@@ -1,10 +1,15 @@
-
-import { useState } from 'react';
-import { Navbar, Offcanvas, Container, Button, Collapse } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Breadcrumb } from "react-bootstrap";
+import {
+  Navbar,
+  Offcanvas,
+  Container,
+  Button,
+  Collapse,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function OpcionesDesplegable() {
-
   const [showMenu, setShowMenu] = useState(false);
   const [showCategories, setShowCategories] = useState(false);
   const [hoveredButton, setHoveredButton] = useState(null);
@@ -26,6 +31,12 @@ function OpcionesDesplegable() {
           <Button variant="outline-dark" onClick={handleShow}>
             <span className="navbar-toggler-icon"></span>
           </Button>
+          <Breadcrumb className="m-3">
+            <Breadcrumb.Item href="/">Página principal</Breadcrumb.Item>
+            <Breadcrumb.Item href="/pedidos">
+              Mis pedidos
+            </Breadcrumb.Item>
+          </Breadcrumb>
         </Container>
       </Navbar>
 
@@ -36,96 +47,148 @@ function OpcionesDesplegable() {
         <Offcanvas.Body>
           <ul style={{ listStyleType: "none", padding: "0" }}>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Inicio')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Inicio")}
                 onMouseLeave={handleMouseLeave}
                 href="/"
-                style={{ backgroundColor: hoveredButton === 'Inicio' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Inicio" ? "gray" : "initial",
+                }}>
                 Inicio
               </Button>
             </li>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Login')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Login")}
                 onMouseLeave={handleMouseLeave}
                 href="/login"
-                style={{ backgroundColor: hoveredButton === 'Login' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Login" ? "gray" : "initial",
+                }}>
                 Login
               </Button>
             </li>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Ver carrito')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Ver carrito")}
                 onMouseLeave={handleMouseLeave}
                 href="/carrito"
-                style={{ backgroundColor: hoveredButton === 'Ver carrito' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Ver carrito" ? "gray" : "initial",
+                }}>
                 Ver carrito
               </Button>
             </li>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Categorías')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Categorías")}
                 onMouseLeave={handleMouseLeave}
                 onClick={handleCategories}
-                style={{ backgroundColor: hoveredButton === 'Categorías' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Categorías" ? "gray" : "initial",
+                }}>
                 Categorías
               </Button>
             </li>
             <Collapse in={showCategories}>
               <ul style={{ listStyleType: "none" }}>
                 <li>
-                  <Button variant="transparent"
-                    onMouseEnter={() => handleMouseEnter('Moda Mujer')}
+                  <Button
+                    variant="transparent"
+                    onMouseEnter={() => handleMouseEnter("Moda Mujer")}
                     onMouseLeave={handleMouseLeave}
-                    onClick={() => { navigate('/?categoria=modaMujer'); handleClose(); }}
-                    style={{ backgroundColor: hoveredButton === 'Moda Mujer' ? 'gray' : 'initial' }}>
+                    onClick={() => {
+                      navigate("/?categoria=modaMujer");
+                      handleClose();
+                    }}
+                    style={{
+                      backgroundColor:
+                        hoveredButton === "Moda Mujer" ? "gray" : "initial",
+                    }}>
                     Moda Mujer
                   </Button>
                 </li>
                 <li>
-                  <Button variant="transparent"
-                    onMouseEnter={() => handleMouseEnter('Moda Hombre')}
+                  <Button
+                    variant="transparent"
+                    onMouseEnter={() => handleMouseEnter("Moda Hombre")}
                     onMouseLeave={handleMouseLeave}
-                    onClick={() => { navigate('/?categoria=modaHombre'); handleClose(); }}
-                    style={{ backgroundColor: hoveredButton === 'Moda Hombre' ? 'gray' : 'initial' }}>
+                    onClick={() => {
+                      navigate("/?categoria=modaHombre");
+                      handleClose();
+                    }}
+                    style={{
+                      backgroundColor:
+                        hoveredButton === "Moda Hombre" ? "gray" : "initial",
+                    }}>
                     Moda Hombre
                   </Button>
                 </li>
                 <li>
-                  <Button variant="transparent"
-                    onMouseEnter={() => handleMouseEnter('Calzado')}
+                  <Button
+                    variant="transparent"
+                    onMouseEnter={() => handleMouseEnter("Calzado")}
                     onMouseLeave={handleMouseLeave}
-                    onClick={() => { navigate('/?categoria=calzado'); handleClose(); }}
-                    style={{ backgroundColor: hoveredButton === 'Calzado' ? 'gray' : 'initial' }}>
+                    onClick={() => {
+                      navigate("/?categoria=calzado");
+                      handleClose();
+                    }}
+                    style={{
+                      backgroundColor:
+                        hoveredButton === "Calzado" ? "gray" : "initial",
+                    }}>
                     Calzado
                   </Button>
                 </li>
                 <li>
-                  <Button variant="transparent"
-                    onMouseEnter={() => handleMouseEnter('Accesorios')}
+                  <Button
+                    variant="transparent"
+                    onMouseEnter={() => handleMouseEnter("Accesorios")}
                     onMouseLeave={handleMouseLeave}
-                    onClick={() => { navigate('/?categoria=accesorios'); handleClose(); }}
-                    style={{ backgroundColor: hoveredButton === 'Accesorios' ? 'gray' : 'initial' }}>
+                    onClick={() => {
+                      navigate("/?categoria=accesorios");
+                      handleClose();
+                    }}
+                    style={{
+                      backgroundColor:
+                        hoveredButton === "Accesorios" ? "gray" : "initial",
+                    }}>
                     Accesorios
                   </Button>
                 </li>
               </ul>
             </Collapse>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Ver pedidos')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Ver pedidos")}
                 onMouseLeave={handleMouseLeave}
                 href="/pedidos"
-                style={{ backgroundColor: hoveredButton === 'Ver pedidos' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Ver pedidos" ? "gray" : "initial",
+                }}>
                 Ver pedidos
               </Button>
             </li>
             <li>
-              <Button variant="transparent"
-                onMouseEnter={() => handleMouseEnter('Sobre nosotros')}
+              <Button
+                variant="transparent"
+                onMouseEnter={() => handleMouseEnter("Sobre nosotros")}
                 onMouseLeave={handleMouseLeave}
                 href="/sobre-nosotros"
-                style={{ backgroundColor: hoveredButton === 'Sobre nosotros' ? 'gray' : 'initial' }}>
+                style={{
+                  backgroundColor:
+                    hoveredButton === "Sobre nosotros" ? "gray" : "initial",
+                }}>
                 Sobre nosotros
               </Button>
             </li>
